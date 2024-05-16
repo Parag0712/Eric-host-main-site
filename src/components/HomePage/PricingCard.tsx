2f5aa8ef828875cb9660469bb875314980b92713
parag0712 <paragvadgama123@gmail.com>
2024-05-16 13:40:43 +0530

    pricing done

---
+++ src/components/HomePage/PricingCard.tsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '../ui/card'
+import { Button } from '../ui/button'
+import { Badge } from '../ui/badge'
+import { Check } from 'lucide-react'
+
+type Props = {}
+
+const PricingCard = ({ pricing, plan }: any) => {
+    const currency = "₹"
+    const year = false;
+    const PlanPrice = year ? plan.monthly : plan.annually;
+    enum PopularPlanType {
+        NO = 0,
+        YES = 1,
+    }
+
+    return (
+        <Card
+            key={plan.title}
+            className={
+                plan.popular === PopularPlanType.YES
+                    ? "drop-shadow-xl shadow-black/10 capitalize dark:shadow-white/10 border-[1px] transition-all duration-150  hover:shadow-xl  hover:border-primary  lg:scale-[1.03]"
+                    : "border-[1px] transition-all duration-150 capitalize hover:shadow-xl  hover:border-primary "
+            }
+        >
+            <CardHeader>
+                <CardTitle className="flex item-center justify-between ">
+                    {plan.title}
+                    {plan.popular === PopularPlanType.YES ? (
+                        <Badge
+                            variant="secondary"
+                            className="text-sm text-primary"
+                        >
+                            Most popular
+                        </Badge>
+                    ) : null}
+                </CardTitle>
+                <div>
+                    <span className="text-3xl font-bold">{currency + PlanPrice}</span>
+                    <span className="text-muted-foreground"> /month</span>
+                </div>
+
+                <CardDescription>{plan.description}</CardDescription>
+            </CardHeader>
+
+            <CardFooter className="flex flex-col justify-start items-start w-full">
+
+                <div className="space-y-4">
+                    {plan.features.map((benefit: any, index: number) => (
+                        <span
+                            key={index}
+                            className="flex"
+                        >
+                            <Check className="text-green-500" />{" "}
+                            <h3 className="ml-2">{benefit.description}</h3>
+                        </span>
+                    ))}
+                </div>
+                <Button className="w-full my-6">Security</Button>
+
+                <div className="space-y-4">
+                    {plan.security.map((benefit: any) => (
+                        <span
+                            key={benefit}
+                            className="flex"
+                        >
+                            <Check className="text-green-500" />{" "}
+                            <h3 className="ml-2">{benefit.description}</h3>
+                        </span>
+                    ))}
+                </div>
+                <Button className="w-full my-6">Support</Button>
+
+                <div className="space-y-4">
+                    {plan.security.map((benefit: any,index) => (
+                        <span
+                            key={index}
+                            className="flex"
+                        >
+                            <Check className="text-green-500" />{" "}
+                            <h3 className="ml-2">{benefit.description}</h3>
+                        </span>
+                    ))}
+                </div>
+                <Button className="w-full my-6">Resources</Button>
+
+                <div className="space-y-4">
+                    {plan.resources.map((benefit: any,index:any) => (
+                        <span
+                            key={index}
+                            className="flex"
+                        >
+                            <Check className="text-green-500" />{" "}
+                            <h3 className="ml-2">{benefit.description}</h3>
+                        </span>
+                    ))}
+                </div>
+                
+            </CardFooter>
+
+        </Card>
+    )
+}
+
+export default PricingCard
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   prefix: "",
   theme: {
     container: {
@@ -27,9 +27,10 @@ const config = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        primary: { DEFAULT: "#4246C9",
-        foreground: "#ffffff",
-        hover: "#2c3195", // Cust
+        primary: {
+          DEFAULT: "#4246C9",
+          foreground: "#ffffff",
+          hover: "#2c3195", // Cust
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
@@ -55,7 +56,7 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
-        ericheading:{
+        ericheading: {
           DEFAULT: "#061C3D",
           foreground: "#061C3D",
         }
@@ -74,10 +75,19 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "background-shine": {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "background-shine": "background-shine 2s linear infinite",
       },
     },
   },
