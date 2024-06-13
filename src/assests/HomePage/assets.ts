e157a56a716086c7ca9d2a5b42331e909c30d543
parag0712 <paragvadgama123@gmail.com>
2024-06-13 12:27:13 +0530

    change image

--- src/assests/HomePage/assets.ts
+++ src/assests/HomePage/assets.ts
@@ -3,11 +3,11 @@ export { default as HeroSection } from '../../../public/Creatives/57.png';
 
 export { default as DigitalPresence } from '../../../public/Creatives/58.png';
 
-export { default as DedicatedSecured } from '../../../public/Creatives/57.png';
+export { default as DedicatedSecured } from '../../../public/Creatives/28.png';
 
 export { default as FreeUnlimited } from '../../../public/Creatives/43.png';
 
-export { default as DedicatedcPanels } from '../../../public/Creatives/28.png';
+export { default as DedicatedcPanels } from '../../../public/Creatives/39.png';
 
 export { default as AIBased } from '../../../public/Creatives/49.png';
 
